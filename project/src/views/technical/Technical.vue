a35e389a3d326fc1bb53c8e23a76914325faa6b3
niceman <370817194@qq.com>
2019-12-03 00:36:09 +0800

    element-ui

--- project/src/views/technical/Technical.vue
+++ project/src/views/technical/Technical.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="technical">
-    <h2>技术管理系统</h2>
+    <h2>技术管理系统233333</h2>
+        <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -10,6 +18,38 @@
   }
 </script>
 
-<style scoped>
-
+<style>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
 </style>
